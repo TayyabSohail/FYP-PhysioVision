d2522a4e2250f4665758da63d8e0a4b5254be322
Tayyab Sohail <m.tayyabsohail614@gmail.com>
2024-11-12 11:19:07 +1000

    feat: components folder for Buttons

---
+++ PhysioVision/src/assets/styles/index.ts
@@ -0,0 +1,26 @@
+export const styles = {
+    heading1:
+      "m-0 p-0 font-lato font-bold text-[48px] leading-[56px] text-primary",
+    heading2:
+      "m-0 p-0 font-lato font-bold text-[32px] leading-[40px] text-primary",
+    heading3:
+      "m-0 p-0 font-lato font-bold text-[24px] leading-[32px] text-primary",
+    heading4:
+      "m-0 p-0 font-lato font-bold text-[20px] leading-[28px] text-primary",
+    heading5:
+      "m-0 p-0 font-lato font-bold text-[18px] leading-[24px] text-primary",
+    heading6: "m-0 p-0 font-lato font-bold text-[16px] leading-[20px] text-black",
+  
+    label: "m-0 p-0 font-semibold !text-[16px] leading-[20px] text-black",
+    disabled: "!text-white !bg-primary !opacity-30",
+  
+    section: "flex flex-col p-10 gap-5",
+    box: "flex flex-col gap-5 p-5 border border-light_gray rounded-lg bg-white",
+    line: "border-t border-light_gray my-0 py-0",
+  
+    tableTags: "font-medium !text-black !text-[14px] py-1 px-3 rounded-lg",
+    filtertags:
+      "font-medium !text-black !text-[14px] px-1 rounded-md border-none",
+    cardTags: "w-4 h-4 mt-1 border-none rounded-full",
+  };
+  
